9a92c5bdd81d2f55243a189cc8ddaf85ee7c95fb
sahilcmd3 <msgsahil5@gmail.com>
2025-03-29 03:13:25 +0530

    New Question

---
+++ Codes/ContainerWithMostWater(LC).py
@@ -0,0 +1,31 @@
+#LEETCODE (medium)
+
+"""You are given an integer array height of length n. There are n vertical lines drawn such that the two
+endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Notice that you may not slant the container."""
+
+
+#Time Complexity: O(n)
+def maxArea(height):
+    max_area = 0
+    left = 0
+    right = len(height) - 1
+
+    while left < right:
+        max_area = max(max_area, (right - left) * min(height[left], height[right]))
+
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+
+    return max_area
+
+
+ans = maxArea(height=[1, 8, 6, 2, 5, 4, 8, 3, 7])
+print(ans)
