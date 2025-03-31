695338ee04eb54aab6289859ced7507934afb5e4
sahilcmd3 <msgsahil5@gmail.com>
2025-04-01 04:44:30 +0530

    New Question

---
+++ Codes/CRT.py
@@ -0,0 +1,119 @@
+# CRT PRACTICE QUESTIONS (First Set)
+
+# Add Two Integers
+def Sum(num1,num2):
+    add = num1 + num2
+    return add
+
+ans = Sum(num1 = -10, num2 = 4)
+print(ans)
+
+
+# Convert Two Temperatures
+def convert(celsius):
+    kelvin = celsius + 273.15
+    fahrenheit = celsius * 1.80 + 32.00
+    return [kelvin, fahrenheit]
+    
+ans = convert(celsius = 36.50)
+print(ans)
+
+
+# Smallest Even Multiple
+def sem(n):
+    if n % 2 == 0:
+        return n
+    else:
+        return n * 2
+
+ans = sem(n = 6)
+print(ans)
+
+
+# Alternating Digit Sum
+def ads(n):
+    s=str(n)
+    pt=0
+
+    for i in range(0,len(s),2):
+        pt+=int(s[i])
+    for i in range(1,len(s),2):
+        pt-=int(s[i])
+
+    return pt
+
+ans=ads(n=521)
+print(ans)
+
+
+# Palindrome
+def palindrome(n):
+    s = str(n)
+    left = 0
+    right = len(s) - 1
+
+    while left < right:
+        if s[left] != s[right]:
+            return False
+
+        left += 1
+        right -= 1
+
+    return True
+
+ans = palindrome(n = 121)
+print(ans)
+
+
+# Prime Number
+# Time Complexity: O(root(n)/6)
+def isprime(n):
+
+    if n<=1:
+        return False
+    elif n<=3:
+        return False
+    elif n%2==0 or n%3==0:
+        return False
+
+    i=5
+    while i*i<=n: # checking 6n+-1
+        if n%i==0 or n%(i+2)==0:
+            return False
+        i+=6
+
+    return True
+
+ans=isprime(n=7)
+print(ans)
+
+
+#Reverse Digits
+def revedigits(n):
+    reverse_str = ""
+    while n > 0:
+        reverse_str += str(n % 10)
+        n //= 10  # Right shift
+    return int(reverse_str)
+
+ans = revedigits(n=122)
+print(ans)
+
+
+# Armstrong Number
+def armsnum(n):
+    temp = n # Store the original number to compare later
+    res = 0 # Variable to store the sum of the cubes of the digits
+
+    while n != 0: # Loop to process each digit of the number
+        digit = n % 10 # Get the last digit of the number
+        res += digit ** 3 # Add the cube of the digit to the result
+        n //= 10 # Remove the last digit from the number
+
+    if res == temp: # Check if the sum of cubes (res) is equal to the original number or not
+        return True
+    else:
+        return False
+
+ans=armsnum(n=153)
+print(ans)
