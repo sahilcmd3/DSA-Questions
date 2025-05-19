24a620b752d2681eaed6e9cf5fc65b1836f691ed
sahilcmd3 <msgsahil5@gmail.com>
2025-05-19 18:48:12 +0530

    New Question

--- Codes/3024DailyChal.py
+++ Codes/3024DailyChal.py
@@ -5,7 +5,8 @@
 A triangle is called equilateral if it has all sides of equal length.
 A triangle is called isosceles if it has exactly two sides of equal length.
 A triangle is called scalene if all its sides are of different lengths.
-Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle."""
+Return a string representing the type of triangle that can be formed or "none" if it cannot form a triangle.
+"""
 
 
 # Time Complexity: O(1)
@@ -13,13 +14,13 @@ Return a string representing the type of triangle that can be formed or "none" i
 def triangletypes(nums):
     nums.sort()
 
-    if nums[2]>=nums[0]+nums[1]:
+    if nums[2] >= nums[0] + nums[1]:
         return None
-    elif nums[0]==nums[2]:
+    elif nums[0] == nums[2]:
         return "equilateral"
-    elif nums[0]==nums[1] or nums[1]==nums[2]:
+    elif nums[0] == nums[1] or nums[1] == nums[2]:
         return "isosceles"
     else:
         return "scalene"
-    
-print(triangletypes(nums=[3,4,5]))
+
+print(triangletypes(nums=[3, 4, 5]))
