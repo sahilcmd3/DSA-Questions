5f7dfc8f161ca66188c99530439bffb903aa07db
sahilcmd3 <msgsahil5@gmail.com>
2025-05-31 18:01:37 +0530

    New Questions

--- Codes/Simplify Path(LC).py
+++ Codes/Simplify Path(LC).py
@@ -16,8 +16,57 @@ The path must start with a single slash '/'.
 Directories within the path must be separated by exactly one slash '/'.
 The path must not end with a slash '/', unless it is the root directory.
 The path must not have any single or double periods ('.' and '..') used to denote current or parent directories.
-Return the simplified canonical path."""
+Return the simplified canonical path.
+
+Example 1:
+
+Input: path = "/home/"
+
+Output: "/home"
+
+Explanation:
+
+The trailing slash should be removed."""
 
 
+# Time Complexity: O(n)
 def simplifyPath(path):
-    pass
+    components=path.split('/')  # Splitting the path using "/" separating "." and ".."
+    st=[]  # stack (Pushing into stack = moving further into directory & Poping means the reverse)
+
+    for i in components:
+        if i=="" or i == ".":  # "." are ignored
+            continue
+        elif i=="..":
+            if st:
+                st.pop()
+        else:
+            st.append(i)
+    
+    return "/"+"/".join(st)
+
+print(simplifyPath(path = "/.../a/../b/c/../d/./"))
+
+
+"""
+Example Walkthrough:
+    Example path:
+    path = "/home/../usr//bin/./script"
+    
+    Initialization:
+    components = ["", "home", "..", "usr", "", "bin", ".", "script"]
+    st = []
+
+    Processing Components:
+    "": Skip.
+    "home": Push to stack: st = ["home"].
+    "..": Pop from stack: st = [].
+    "usr": Push to stack: st = ["usr"].
+    "": Skip.
+    "bin": Push to stack: st = ["usr", "bin"].
+    ".": Skip.
+    "script": Push to stack: st = ["usr", "bin", "script"].
+
+    Final Result:
+    "/usr/bin/script"
+"""
