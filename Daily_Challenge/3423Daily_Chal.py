e62559b9cf70724753fe72fd08dc6771c909141f
sahilcmd3 <msgsahil5@gmail.com>
2025-06-13 00:18:32 +0530

    New Question

---
+++ Daily_Challenge/3423Daily_Chal.py
@@ -0,0 +1,36 @@
+# LEETCODE
+
+"""Given a circular array nums, find the maximum absolute difference between adjacent elements.
+
+Note: In a circular array, the first and last elements are adjacent.
+
+
+Example 1:
+Input: nums = [1,2,4]
+Output: 3
+
+Explanation:
+Because nums is circular, nums[0] and nums[2] are adjacent. They have the maximum absolute difference of |4 - 1| = 3.
+
+
+Example 2:
+Input: nums = [-5,-10,-5]
+Output: 5
+
+Explanation:
+The adjacent elements nums[0] and nums[1] have the maximum absolute difference of |-5 - (-10)| = 5.
+"""
+
+
+# Time complexity: O(n)
+def maxAdjDis(nums):
+    n = len(nums)
+    maxadj = abs(nums[0] - nums[-1])
+
+    for i in range(n - 1):
+        maxadj = max(maxadj, abs(nums[i] - nums[i + 1]))
+
+    return maxadj
+
+
+print(maxAdjDis(nums=[-5, -10, -5]))
