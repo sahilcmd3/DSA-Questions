9f0bc726114b5872273c1e5c219714a329b6b9c6
sahilcmd3 <msgsahil5@gmail.com>
2025-06-20 02:23:09 +0530

    New Question

--- HackerRank/ICPC Team.py
+++ HackerRank/ICPC Team.py
@@ -1,7 +1,7 @@
-"""There are a number of people who will be attending ACM-ICPC World Finals. Each of them may be well versed in a number of topics. 
-Given a list of topics known by each attendee, presented as binary strings, determine the maximum number of topics a 2-person team can know. 
-Each subject has a column in the binary string, and a '1' means the subject is known while '0' means it is not. Also determine the number of 
-teams that know the maximum number of topics. Return an integer array with two elements. The first is the maximum number of topics known, 
+"""There are a number of people who will be attending ACM-ICPC World Finals. Each of them may be well versed in a number of topics.
+Given a list of topics known by each attendee, presented as binary strings, determine the maximum number of topics a 2-person team can know.
+Each subject has a column in the binary string, and a '1' means the subject is known while '0' means it is not. Also determine the number of
+teams that know the maximum number of topics. Return an integer array with two elements. The first is the maximum number of topics known,
 and the second is the number of teams that know that number of topics."""
 
 
@@ -12,7 +12,7 @@ def acmTeam(topic):
     for i in range(len(topic)):
         for j in range(i + 1, len(topic)):
             combined = bin(int(topic[i], 2) | int(topic[j], 2))
-            topics_known = combined.count('1')
+            topics_known = combined.count("1")
 
             if topics_known > max_topics:
                 max_topics = topics_known
@@ -24,4 +24,4 @@ def acmTeam(topic):
 
 
 if __name__ == "__main__":
-    print(acmTeam(topic=['10101', '11110', '00010']))
+    print(acmTeam(topic=["10101", "11110", "00010"]))
