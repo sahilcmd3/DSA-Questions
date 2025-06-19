617e62ddfae70dcb24e8f8389872528e47896d1b
sahilcmd3 <msgsahil5@gmail.com>
2025-06-20 02:22:43 +0530

    New Question

--- CRT_2/Binary Search.py
+++ CRT_2/Binary Search.py
@@ -158,6 +158,40 @@ def koko(piles, h):
             left = mid + 1  # Need a larger speed
     
     return left
+
+
+
+# LEETCODE 1011
+"""A conveyor belt has packages that must be shipped from one port to another within days days.
+The ith package on the conveyor belt has a weight of weights[i]. Each day, we load the ship with packages on the conveyor 
+belt (in the order given by weights). We may not load more weight than the maximum weight capacity of the ship.
+Return the least weight capacity of the ship that will result in all the packages on the conveyor belt being shipped within days days."""
+
+# Time complexity: O(N∗Log(Sum(Weights)))
+def shipwithinDays(weights, days):
+    def can_Ship(capacity):
+        total = 0
+        d = 1
+        
+        for w in weights:
+            if total + w > capacity:
+                d += 1
+                total = 0
+            
+            total += w
+        
+        return d <= days
+    
+    left, right = max(weights), sum(weights)
+    while left < right:
+        mid = left + (right - left) // 2
+        
+        if can_Ship(mid):
+            right = mid
+        else:
+            left = mid + 1
+        
+    return left
         
     
 if __name__ == "__main__":
@@ -165,4 +199,5 @@ if __name__ == "__main__":
     #print(sip(nums=[1, 3, 5, 6], target=5))
     #print(mysqrt(x=8))
     #print(peakMIA(arr = [0,1,0]))
-    print(koko(piles = [3,6,7,11], h = 8))
+    #print(koko(piles = [3,6,7,11], h = 8))
+    print(shipwithinDays(weights=[1,2,3,4,5,6,7,8,9,10], days = 5))
