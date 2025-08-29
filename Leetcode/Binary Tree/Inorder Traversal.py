6c9171a28736cbc74e238a87d00ba3996245fdea
Sahil Dwivedi <msgsahil5@gmail.com>
2025-08-29 22:43:33 +0530

    New Question

--- Leetcode/Binary Tree/Inorder Traversal.py
+++ Leetcode/Binary Tree/Inorder Traversal.py
@@ -2,10 +2,6 @@
 
 """Given the root of a binary tree, return the inorder traversal of its nodes' values."""
 
-# Iteratively khud krna h (stack)
-# Recursive approach
-# L N R
-
 
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
@@ -14,16 +10,35 @@ class TreeNode:
         self.right = right
 
 
+# Recursive approach
+# L N R
 def inorderTrav(root):
     result = []
 
     def inorder(node):
-        if not node:
+        if not node:  # Base case to stop the recursive traversing (also means reaching the end of a subtree)
             return
 
-        inorder(node.left)
-        result.append(node.val)
-        inorder(node.right)
+        inorder(node.left)  # L
+        result.append(node.val)  # N (ye append isliye hua hai ku ki subtrees ka root node hi main ya left node hota hai)
+        inorder(node.right)  # R
 
     inorder(root)
     return result
+
+
+# Iteratively (stack)
+def inorder(root):
+    res = []
+    stack = []
+
+    while root or stack:
+        while root:
+            stack.append(root)
+            root = root.left
+
+        root = stack.pop()
+        res.append(root.val)
+        root = root.right
+
+    return res
