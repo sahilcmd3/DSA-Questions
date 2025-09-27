62fc06a6f06f2c40a9eb47eb04c2d10a44b3f256
Sahil Dwivedi <msgsahil5@gmail.com>
2025-09-27 19:03:09 +0530

    New Question

---
+++ Leetcode/Strings/Leetcode 1021.py
@@ -0,0 +1,41 @@
+# Remove Outermost Parenthesis
+
+"""A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents
+string concatenation.
+
+    For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+
+A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B
+nonempty valid parentheses strings.
+Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses
+strings.
+Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+"""
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        """
+        Removes the outermost parentheses of every primitive string in the decomposition.
+        """
+        res = []
+        opened_count = 0
+
+        for char in s:
+            if char == '(':
+                if opened_count > 0:
+                    res.append(char)
+                opened_count += 1
+            else:
+                opened_count -= 1
+                if opened_count > 0:
+                    res.append(char)
+
+        return "".join(res)
+    
+
+if __name__ == "__main__":
+    obj = Solution()
+    print(obj.removeOuterParentheses(s="(()())(())"))
+    print(obj.removeOuterParentheses(s="(()())(())(()(()))"))
+    print(obj.removeOuterParentheses(s="()()"))
